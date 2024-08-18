d3ab6b5f142045dc9fd5ac8c24ef6bbdc749ad14
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-18 00:44:53 -0300

    ✨ (AppConfig.java): adiciona classe de configuração para o aplicativo com um bean para o PasswordEncoder ✨ (AuthorizationServerConfig.java): adiciona classe de configuração para o servidor de autorização com configurações detalhadas para OAuth2 ✨ (ResourceServerConfig.java): adiciona classe de configuração para o servidor de recursos com configurações detalhadas para segurança e CORS
    As alterações foram feitas para adicionar classes de configuração para o
    aplicativo, servidor de autorização e servidor de recursos, a fim de
    configurar adequadamente a segurança, autenticação e autorização usando
    OAuth2, bem como configurar o CORS para permitir solicitações de diferentes
    origens.
    
    ✨ Adiciona classes para suporte a autenticação personalizada via senha
    
    As classes CustomPasswordAuthenticationConverter, CustomPasswordAuthenticationProvider e CustomPasswordAuthenticationToken foram adicionadas para oferecer suporte à autenticação personalizada via senha. Essas classes permitem a validação de credenciais de usuário, geração de tokens de acesso e autorização de escopos, melhorando a segurança e flexibilidade do sistema de autenticação.
    
    ✨ (CustomUserAuthorities.java): Adiciona classe CustomUserAuthorities para representar
    as autoridades personalizadas de um usuário no sistema de autenticação.
    🔧 (RoleRepository.java): Adiciona a anotação @Repository para indicar que a interface
    RoleRepository é um repositório Spring.
    📝 (import.sql): Adiciona dados de exemplo para usuários, papéis e associações
    usuário-papel no arquivo de importação SQL.
    Essas alterações foram feitas para adicionar suporte a autoridades personalizadas
    para usuários no sistema de autenticação, bem como para adicionar dados de exemplo
    para usuários, papéis e associações usuário-papel no banco de dados.

---
+++ src/main/java/com/devsuperior/demo/config/customgrant/CustomUserAuthorities.java
@@ -0,0 +1,28 @@
+package com.devsuperior.demo.config.customgrant;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class CustomUserAuthorities {
+
+	private final String username;
+	private Collection<? extends GrantedAuthority> authorities;
+
+	public CustomUserAuthorities(String username, Collection<? extends GrantedAuthority> authorities) {
+		this.username = username;
+		this.authorities = authorities;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+		this.authorities = authorities;
+	}
+}
--- src/main/java/com/devsuperior/demo/repositories/RoleRepository.java
+++ src/main/java/com/devsuperior/demo/repositories/RoleRepository.java
@@ -2,6 +2,8 @@ package com.devsuperior.demo.repositories;
 
 import com.devsuperior.demo.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
